8335ac48726ceede74e4363ca23b9b9be75de5da
Parul101shah <97349441+Parul101shah@users.noreply.github.com>
2026-08-31 22:41:47 +0530

    Initial project setup

---
+++ src/main/java/org/example/jobautomation/JobAutomationApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobAutomationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jobautomation/JobAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
